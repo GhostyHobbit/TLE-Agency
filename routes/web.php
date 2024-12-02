9c66f0a4763ab69c80b2bd23ce4e70ed476e8e57
Geert van der Velden <1079252@hr.nl>
2024-12-02 14:38:04 +0100

    add map structure

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('test', function () {
+    return view('employers.sendmessage');
+});
+
 require __DIR__.'/auth.php';
