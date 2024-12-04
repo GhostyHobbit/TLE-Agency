6f39c157c821c09d14f8ebb992e7caaf6b5b4c6c
wesle <wesleynederpel43@gmail.com>
2024-12-04 10:41:42 +0100

    ADD description and path columns to vacancies db table

---
+++ database/migrations/2024_12_04_093305_add_columns_to_vacancies_table--table=vacancies.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            $table->string('description')->nullable()->after('salary');
+            $table->string('path')->nullable()->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            $table->dropColumn('description');
+            $table->dropColumn('path');
+        });
+    }
+};
