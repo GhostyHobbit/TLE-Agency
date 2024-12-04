b84f5a1f75ffd71b85605f66c955ee428a55d7de
wesle <wesleynederpel43@gmail.com>
2024-12-04 10:28:10 +0100

    MADE grid and css for vacancy overview

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -1,8 +1,8 @@
 <x-nav>
-    <div id="vacancies" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div id="vacancies" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         @foreach($vacancies as $vacancy)
-            <div class="bg-[#92AA83] border border-[#2E342A] shadow-md rounded-lg p-4">
-                <h3 class="text-xl font-bold mb-2">{{ $vacancy->name }}</h3>
+            <div class="bg-[#E2ECC8] border border-[#2E342A] shadow-md rounded-lg p-4">
+                <h3 class="text-[#2E342A] text-xl font-bold m-2">{{ $vacancy->name }}</h3>
                 <p class="text-[#2E342A] mb-2">Employer: {{ $vacancy->employer->name }}</p>
                 <p class="text-[#2E342A] mb-2">Hours: {{ $vacancy->hours }}</p>
                 <p class="text-[#2E342A] mb-2">Salary: {{ $vacancy->salary }}</p>
