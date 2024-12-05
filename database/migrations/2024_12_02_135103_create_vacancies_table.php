b9e4c4c45f74d346076762ff533e97c8de2df177
sildatema <sildatema04@gmail.com>
2024-12-05 08:37:50 +0100

    database makeup flow change

--- database/migrations/2024_12_02_135103_create_vacancies_table.php
+++ database/migrations/2024_12_02_135103_create_vacancies_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->integer('hours');
             $table->integer('salary');
-            $table->string('location');
-            $table->string('Description');
-            $table->string('path');
             $table->timestamps();
         });
     }
