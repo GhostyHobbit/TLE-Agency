bf3a01ba2f524134753a45ec5208b4d150995c41
sildatema <sildatema04@gmail.com>
2024-12-09 14:55:56 +0100

    checkin, made page for welcome and privacy policy, standard css aplied, links to different parts of the code

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
