1d309a8971ff8a6245f48ac472af9fe354a1c547
vharixce <1073901@hr.nl>
2024-12-11 16:31:34 +0100

    profile pic begin

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,4 @@
-
-
-<x-app-layout>
+<x-nav>
     <x-slot name="header">
         <h2>
             {{ __('Profile') }}
@@ -21,10 +19,13 @@
                 </div>
             </div>
             <div class="mb-4">
-                <a  style="color: white;" href="{{ route('profile.preferences') }}">
+                <a href="{{ route('profile.preferences') }}" class="w-[296px] h-[53px] px-[44.50px] py-4 bg-[#aa0160] rounded-2xl border-b-4 border-[#7c1a51] justify-center items-center inline-flex text-[#fbfcf6] text-base font-bold font-['Radikal'] leading-snug">
                     {{ __('Profile Preferences') }}
+
+
                 </a>
             </div>
+
             <div>
                 <div>
                     @include('profile.partials.delete-user-form')
@@ -32,4 +33,10 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+    @if (session('status'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+
+</x-nav>
