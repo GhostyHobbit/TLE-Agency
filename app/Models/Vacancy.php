d4cb5fb9930c262979a71165fe6ad03a661fdab4
Geert van der Velden <1079252@hr.nl>
2024-12-12 11:40:03 +0100

    Fix line endings

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -6,9 +6,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Vacancy extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'employer_id', 'hours', 'salary'];
 
     public function employees()
@@ -18,8 +20,16 @@ class Vacancy extends Model
             ->withTimestamps();
     }
 
-    public function employer(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+
+    public function employer()
     {
         return $this->belongsTo(Employer::class);
     }
+
+
 }
