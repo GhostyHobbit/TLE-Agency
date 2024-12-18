be1c54e5c3f3862ddd6b851400fbccb19caa90f5
wesle <wesleynederpel43@gmail.com>
2024-12-18 15:09:57 +0100

    Final bug fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CompanySeeder::class,
+            EmployerSeeder::class,
             VacancySeeder::class
         ]);
     }
